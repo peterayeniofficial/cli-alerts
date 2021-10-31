2a2c1eead6a2634296072c60bd24ac363ff9af47
Peter Ayeni <ayenicology@gmail.com>
2021-10-31 20:01:50 +0000

    👌 IMPROVE: add cli fuction

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const chalk = require('chalk');
 const sym = require('log-symbols');
 
-module.exports = () => {
-	console.log('working');
+const redI = chalk.red.inverse;
+const greenI = chalk.green.inverse;
+const yellowI = chalk.yellow.inverse;
+const blueI = chalk.blue.inverse;
+const red = chalk.red;
+const green = chalk.green;
+const yellow = chalk.yellow;
+const blue = chalk.blue;
+
+module.exports = options => {
+	const defaultOptions = {
+		type: `error`,
+		msg: `You forgot to specify all options`,
+		name: ``
+	};
+	const opts = {...defaultOptions, ...options};
+	const {type, msg, name} = opts;
+
+	const printName = name ? name : type.toUpperCase();
+
+	if (type === `error`) {
+		console.log(`\n${sym.error} ${redI(` ${printName} `)} ${red(msg)}\n`);
+	} else if (type === `success`) {
+		console.log(`\n${sym.success} ${greenI(` ${printName} `)} ${green(msg)}\n`);
+	} else if (type === `warning`) {
+		console.log(`\n${sym.warning} ${yellowI(` ${printName} `)} ${yellow(msg)}\n`);
+	} else if (type === `info`) {
+		console.log(`\n${sym.info} ${blueI(` ${printName} `)} ${blue(msg)}\n`);
+	}
 };
